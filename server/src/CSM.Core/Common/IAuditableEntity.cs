cf2415eacda20d94228b922d643afcba832cf365
tuantai0708 <tai.bang157@gmail.com>
2025-07-01 16:41:28 +0700

    feat(domain): To build core domain related to only send post in main

--- server/src/CSM.Core/Common/IAuditableEntity.cs
+++ server/src/CSM.Core/Common/IAuditableEntity.cs
@@ -2,9 +2,9 @@ namespace CSM.Core.Common;
 
 public interface IAuditableEntity
 {
-    public long CreatedAt { get; set; }
+    public long CreatedAt { get; }
     
-    public long? UpdatedAt { get; set; }
+    public long? UpdatedAt { get; }
     
-    public long? DeletedAt { get; set; }
+    public long? DeletedAt { get; }
 }
